5ffee404526cc8fe1ae60514af7bf3b43b5c90c4
Carlos Weege <118016303+carlosweege@users.noreply.github.com>
2024-04-30 05:06:48 -0300

    Add files via upload

---
+++ DesafioControleFluxo/src/Contador.java
@@ -0,0 +1,25 @@
+import java.util.Scanner;
+public class Contador {
+	public static void main(String[] args) {
+		Scanner terminal = new Scanner(System.in);
+		System.out.println("Digite o primeiro parametro");
+		int parametroUm = terminal.nextInt();
+		System.out.println("Digite o segundo parametro");
+		int parametroDois = terminal.nextInt();
+		
+		try {
+                    contar(parametroUm, parametroDois);
+                }catch (ParametrosInvalidosException exception) {
+                    System.out.println("O segundo parametro deve ser maior que o primeiro");		
+                }
+		
+	}
+	static void contar(int parametroUm, int parametroDois ) throws ParametrosInvalidosException {
+		if(parametroUm > parametroDois)
+                    throw new ParametrosInvalidosException();
+		int contagem = parametroDois - parametroUm;
+		for(int i = 0; i < contagem; i++){
+                    System.out.println("Imprimindo o numero: " +(i+1));
+                }
+	}
+}
